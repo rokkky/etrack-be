2801bd71a0d0af175af00f07f2da5e633e54c4e9
rokkky <ka.egor1999@gmail.com>
2023-10-10 17:26:00 +0300

    profile api, auth middleware

--- src/graphql/resolvers/user.resolver.js
+++ src/graphql/resolvers/user.resolver.js
@@ -1,10 +1,24 @@
 import md5 from 'md5';
 import User from '../../models/user.model.js';
-import { generateAuthTokens } from '../../services/auth.service.js';
+import { generateAuthTokens, verifyToken } from '../../services/auth.service.js';
+import Token from '../../models/token.model.js';
+import { combineResolvers } from 'graphql-resolvers';
+import { isAuthenticated } from '../../services/auth.service.js';
+import { generateError } from '../../utils/graphql-errors.js';
 
 export default {
+  Query: {
+    getUser: combineResolvers(isAuthenticated, async (_, { id }) => {
+      try {
+        const user = await User.findById(id);
+        return user;
+      } catch (e) {
+        return e;
+      }
+    }),
+  },
   Mutation: {
-    logIn: async (_, { input: { email, password }} ) => {
+    logIn: async (_, { input: { email, password } }) => {
       try {
         const hashedPass = md5(password + process.env.PASSWORD_SALT);
         const user = await User.findOne({ password: hashedPass, email });
@@ -28,6 +42,32 @@ export default {
           registrationDate: new Date(),
         });
         return true;
+      } catch (e) {
+        return e;
+      }
+    },
+    changeUsername: combineResolvers(isAuthenticated, async (_, { id, username }) => {
+      try {
+        const user = await User.findByIdAndUpdate(id, { username }, {new: true});
+        return user;
+      } catch (e) {
+        return e;
+      }
+    }),
+    refreshToken: async (_, { token }) => {
+      try {
+        const user = await verifyToken(token);
+        const refreshToken = await Token.findOne({
+          token,
+          user: user.id,
+        });
+        if (!refreshToken) {
+          generateError('User with this token not found', 'TOKEN_NOT_FOUND', 404);
+        } else {
+          await Token.deleteOne({ id: refreshToken.id });
+          const tokens = await generateAuthTokens(user);
+          return tokens;
+        }
       } catch (e) {
         return e;
       }
--- src/graphql/schemas/user.schema.js
+++ src/graphql/schemas/user.schema.js
@@ -1,12 +1,25 @@
 export default `#graphql
+    extend type Query {
+        getUser(id: ID): User!
+    }
     extend type Mutation {
         logIn(input: LogInInput): Token!
         signUp(input: SignUpInput): Boolean!
+        refreshToken(token: String): Token!
+        changeUsername(id: ID!, username: String!): User!
     }
 
     type Token {
         accessToken: String!
         refreshToken: String!
+    }
+
+    type User {
+        id: ID!
+        username: String
+        email: String!
+        password: String!
+        registrationDate: Date!
     }
 
     input SignUpInput {
