be14f426541bd3608b095b26fc797a6894873d23
rokkky <ka.egor1999@gmail.com>
2023-11-27 14:55:00 +0300

    create category mutation, transaction query upd

--- src/graphql/resolvers/transaction.resolver.js
+++ src/graphql/resolvers/transaction.resolver.js
@@ -2,20 +2,24 @@ import { combineResolvers } from 'graphql-resolvers';
 import { isAuthenticated } from '../../services/auth.service.js';
 import Transaction from '../../models/transaction.model.js';
 import { filterTransactions } from '../../utils/transactions-filter.js';
+import { escapeRegex } from '../../utils/escapeRegex.js';
 
 export default {
   Query: {
     getTransactions: combineResolvers(isAuthenticated, async (_, { input: { user, type, filter } }) => {
-      const { sortOptions, searchReq, pageSize, pageIndex, period } = filter;
-      let transactions = await Transaction.find({
-        user,
-        type,
-        description: { $regex: searchReq, $options: 'i' },
-      })
-        .populate('category')
-        .sort(sortOptions.direction ? [[sortOptions.active, sortOptions.direction]] : null);
-
-      return filterTransactions(transactions, pageSize, pageIndex, period);
+      try {
+        const { sortOptions, searchReq, pageSize, pageIndex, period } = filter;
+        let transactions = await Transaction.find({
+          user,
+          type,
+          description: { $regex: escapeRegex(searchReq), $options: 'i' },
+        })
+          .populate('category')
+          .sort(sortOptions.direction ? [[sortOptions.active, sortOptions.direction]] : null);
+        return filterTransactions(transactions, pageSize, pageIndex, period);
+      } catch (e) {
+        return e;
+      }
     }),
   },
   Mutation: {
--- src/graphql/schemas/category.schema.js
+++ src/graphql/schemas/category.schema.js
@@ -2,11 +2,20 @@ export default `#graphql
     extend type Query {
         getUserCategories(userId: ID): [Category!]
     }
+    extend type Mutation {
+        createCategory(input: CreateCategoryInput): Category!
+    }
+
+    input CreateCategoryInput {
+        name: String!
+        user: ID!
+        type: String!
+    }
 
     type Category {
         id: ID!
         name: String!
-        user: User!
+        user: ID!
         type: String!
     }
 `;
